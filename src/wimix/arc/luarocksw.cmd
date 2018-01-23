134d9a4029f5923a7af84c55493e8f4b7b55fda9
tDwtp <Dark-player@gmx.de>
2018-01-23 18:23:37 +0100

    luarocks templates
    LuaRocks version dispatch templates.

--- src/wimix/arc/luarocksw.cmd
+++ src/wimix/arc/luarocksw.cmd
@@ -1,8 +1,22 @@
-@setlocal
-@set /p LUA_PROFILE=<%~pds0\default-version.txt >nul 2>nul
-@if not defined LUA_PROFILE set LUA_PROFILE=53
-@if not exist "%~pds0\..\%LUA_PROFILE%" set LUA_PROFILE=53
-@if "%LUA_PROFILE%" == "wimix" set LUA_PROFILE=53
-@if not exist %~dps0\..\wimix\rocks\%1\%~n0%1.bat @%~dps0\..\wimix\rocks\%LUA_PROFILE%\%~n0%LUA_PROFILE% %*
-@endlocal
-@if     exist %~dps0\..\wimix\rocks\%1\%~n0%1.bat @%~dps0\..\wimix\rocks\%1\%~n0%*
+@ECHO OFF
+GOTO HANDLE
+
+:APPEND_PATH
+FOR %%# IN (%~n0%1.bat) DO IF NOT "%%~$PATH:#" == "" GOTO END
+PUSHD .
+CD /D "%~dp0\rocks\%1"
+SET "Path=%Path%;%CD%"
+POPD
+GOTO END
+
+:HANDLE
+IF NOT EXIST "%~dp0\rocks\%1\%~n0%1.bat" GOTO USEDEFAULT
+CALL :APPEND_PATH %1
+%~n0%*
+GOTO END
+
+:USEDEFAULT
+CALL :APPEND_PATH %1
+%~n053 %*
+
+:END
