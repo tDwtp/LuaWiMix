681f9c941f8f011932fa4fd1e058ee6773a0cdff
tDwtp <Dark-player@gmx.de>
2018-01-24 08:37:52 +0100

    removed the old dispatcher
    Removed the old dispatcher
    Again... I forgot wlua

--- src/wlua.cmd
+++
@@ -1,6 +0,0 @@
-@FOR /F "TOKENS=1 USEBACKQ" %%# IN (`CSCRIPT //NOLOGO %~dps0\wimix\iser\findVer.vbs %*`) DO @SET "LUA_PROFILE=%%#"
-@IF "%LUA_PROFILE%" == "%~1" (
-	@ENDLOCAL & @%~dp0%~1\%~n0%*
-) ELSE (
-	@ENDLOCAL & @%~dp0%LUA_PROFILE%\%~n0%LUA_PROFILE% %*
-)
